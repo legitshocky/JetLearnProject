2ca13414ffa96540f53a94293cb87a35eeb185f4
legitshocky <sourav.pal@jet-learn.com>
2026-02-06 21:21:25 +0530

    added fetchjlid added 2 parent

--- Code.js
+++ Code.js
@@ -341,7 +341,7 @@ function getSystemHealth() {
     return { error: error.message };
   }
 }
-const APP_VERSION = "211"; 
+const APP_VERSION = "212"; 
 
 function getAppVersion() {
   return APP_VERSION;
